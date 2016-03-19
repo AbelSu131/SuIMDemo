9ba8de7bdb9373342be983c5a484086fd9d66546
AbelSu <abelsu1993@outlook.com>
2016-03-19 16:25:47 +0800

    5
    5

--- SuIMDemo/ViewController.m
+++ SuIMDemo/ViewController.m
@@ -7,8 +7,17 @@
 //
 
 #import "ViewController.h"
+#import "NIMSDK.h"
 
-@interface ViewController ()
+@interface ViewController ()<NIMChatManagerDelegate>
+
+@property (nonatomic,strong) UIButton *loginBtn;
+@property (nonatomic,strong) UILabel *actTitleLabel;
+@property (nonatomic,strong) UILabel *pwdTitleLabel;
+@property (nonatomic,strong) UITextField *accountTextField;
+@property (nonatomic,strong) UITextField *pwdTextField;
+@property (nonatomic,strong) UIImageView *imageView;
+@property (nonatomic,strong) UILabel *accountLabel;
 
 @end
 
@@ -16,7 +25,19 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.loginBtn = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [self.loginBtn setTitle:@"登  录" forState:UIControlStateNormal];
+    self.loginBtn.frame = CGRectMake(120, 300, 160, 30);
+    [self.loginBtn.layer setCornerRadius:10];
+    [self.loginBtn.layer setBorderWidth:.5f];
+    [self.view addSubview:self.loginBtn];
+    [self.loginBtn addTarget:self action:@selector(login:) forControlEvents:UIControlEventTouchUpInside];
+    
+    self.actTitleLabel = [[UILabel alloc]initWithFrame:CGRectMake(50, 150, 60, 30)];
+    self.actTitleLabel.text = @"账  号";
+    [self.view addSubview:self.actTitleLabel];
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
